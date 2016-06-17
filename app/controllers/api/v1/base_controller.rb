718ffdf1a9bff5a05dbd618147b7f83fb3d50095
PTonin <xujiguang@ecloudinfo.com>
2016-06-17 15:09:17 +0800

    init the project

---
+++ app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::BaseController < ApplicationController
+  protect_from_forgery with: :null_session
+  before_action :destroy_session
+  skip_before_action :verify_authenticity_token
+
+  private
+  def destroy_session
+    request.session_options[:skip] = true
+  end
+
+  def api_error(opts={})
+    render :nothing=>true, status: opts[:status]
+  end
+end
